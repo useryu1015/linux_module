bb0f98c030956ed05e2db69514b8f59857ff63ed
useryu1015 <2225469536@qq.com>
2024-03-15 10:49:25 +0800

    shell

--- 5.DOS/echo_procinfo.sh
+++ 5.DOS/echo_procinfo.sh
@@ -5,6 +5,7 @@ if [ -z "$write_file" ]; then
     write_file="/dev/tty"
 fi
 
+proc_name="ditoo"
 
 echo 当前时间： > ${write_file}; date >> ${write_file}; echo ------ >> ${write_file};
 
@@ -12,7 +13,7 @@ echo 内存使用情况： >> ${write_file}; free -h >> ${write_file}; echo ----
 
 echo 磁盘使用情况： >> ${write_file}; df -h >> ${write_file}; echo ------ >> ${write_file};
 
-echo 在线进程信息： >> ${write_file}; ps | grep ditoo >> ${write_file}; echo ------ >> ${write_file};
-# echo 在线进程信息： >> ${write_file}; ps -ef | grep ditoo >> ${write_file}; echo ------ >> ${write_file};
+echo 在线进程信息： >> ${write_file}; ps | grep ${proc_name} >> ${write_file}; echo ------ >> ${write_file};
+# echo 在线进程信息： >> ${write_file}; ps -ef | grep ${proc_name} >> ${write_fise}; echo ------ >> ${write_file};
 
-echo 网络连接状态： >> ${write_file}; netstat -natp | grep ditoo >> ${write_file}; echo ------ >> ${write_file};
+echo 网络连接状态： >> ${write_file}; netstat -natp | grep ${proc_name} >> ${write_file}; echo ------ >> ${write_file};
